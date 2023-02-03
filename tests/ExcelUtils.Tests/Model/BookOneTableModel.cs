c0fea486be4227082316d8b681ee244ced7aec18
GuilhermeBley <guilhermecbley@gmail.com>
2023-02-03 19:40:26 -0300

    excel tests

---
+++ tests/ExcelUtils.Tests/Model/BookOneTableModel.cs
@@ -0,0 +1,10 @@
+namespace ExcelUtils.Tests.Model;
+
+public class BookOneTableModel
+{
+    public DateTime? Column1 { get; set; }
+    public string? Column2 { get; set; }
+    public int? Column3 { get; set; }
+    public double? Column4 { get; set; }
+
+}
---
+++ tests/ExcelUtils.Tests/TestsWithExcelFiles.cs
@@ -0,0 +1,41 @@
+using ExcelUtils.Tests.Model;
+using System.Data;
+
+namespace ExcelUtils.Tests;
+
+public class TestsWithExcelFiles
+{
+    public static string CurrentProject { get; } = Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.FullName 
+        ?? throw new ArgumentNullException("Root not found.");
+
+    public static string Root => CurrentProject + "\\root\\";
+
+    [Fact]
+    public void ConvertExcelToDataTable_GetTableFromWorkSheet_Success()
+    {
+        var dt = ExcelRead.ConvertExcelToDataTable(GetRootFile("BookOneTable.xlsx"));
+
+        Assert.NotEmpty(dt.AsEnumerable());
+    }
+
+    [Fact]
+    public void ConvertExcel_GetTableFromWorkSheet_Success()
+    {
+        var modelEnumerable = ExcelRead.ConvertExcel<BookOneTableModel>(GetRootFile("BookOneTable.xlsx"));
+
+        Assert.NotEmpty(modelEnumerable);
+        
+        Assert.Collection(modelEnumerable, (model) =>
+        {
+            Assert.NotNull(model.Column1);
+            Assert.NotNull(model.Column2);
+            Assert.NotNull(model.Column3);
+            Assert.NotNull(model.Column4);
+        });
+    }
+
+    public static string GetRootFile(string fileName)
+    {
+        return Root + fileName;
+    }
+}
