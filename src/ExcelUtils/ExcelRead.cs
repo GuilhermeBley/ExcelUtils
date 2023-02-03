d9b1c5dda966b784266bfc2d515c80b2c8014f75
GuilhermeBley <guilhermecbley@gmail.com>
2023-02-03 19:40:37 -0300

    excel methods

--- src/ExcelUtils/ExcelRead.cs
+++ src/ExcelUtils/ExcelRead.cs
@@ -1,6 +1,6 @@
 ﻿using System.Data;
-using System.Data.OleDb;
 using System.Text.Json;
+using IronXL;
 
 namespace ExcelUtils;
 
@@ -8,34 +8,55 @@ public class ExcelRead
 {
     public const string DefaultTable = "Sheet1$";
 
-    public static async Task<IEnumerable<T>> ConvertExcel<T>(string fullPath, string tableName = DefaultTable)
+    public static async Task<IEnumerable<T>> ConvertExcelAsync<T>(string fullPath, string tableName = DefaultTable)
     {
-        var dataTable = await ConvertExcelToDataTable(fullPath, tableName);
+        var dataTable = await ConvertExcelToDataTableAsync(fullPath, tableName);
 
-        string serializeddt = JsonSerializer.Serialize(dataTable);
+        string serializeddt = SerializeDataTable(dataTable);
 
-        return JsonSerializer.Deserialize<IEnumerable<T>>(serializeddt) ??
+        if (string.IsNullOrEmpty(serializeddt))
+            return Enumerable.Empty<T>();
+
+        return JsonSerializer.Deserialize<IEnumerable<T>>(serializeddt)?.ToList() ??
             throw new ArgumentNullException("Deserialize failed.");
     }
 
-    public static async Task<IEnumerable<T>> ConvertExcel<T>(string path, string filename, string tableName = DefaultTable)
-        => await ConvertExcel<T>(SplitedPathToFull(path, filename), tableName: tableName);
+    public static async Task<IEnumerable<T>> ConvertExcelAsync<T>(string path, string filename, string tableName = DefaultTable)
+        => await ConvertExcelAsync<T>(SplitedPathToFull(path, filename), tableName: tableName);
     
-
-    public static async Task<DataTable> ConvertExcelToDataTable(string fullPath, string tableName = DefaultTable)
+    public static async Task<DataTable> ConvertExcelToDataTableAsync(string fullPath, string tableName = DefaultTable)
     {
-        using OleDbConnection connection = new OleDbConnection(ConvertToConnectionStringOleDb(fullPath));
-        await connection.OpenAsync();
+        var workBook = WorkBook.Load(fullPath);
 
-        using var objDA = new System.Data.OleDb.OleDbDataAdapter($"select * from [{tableName}]", connection);
-        var excelDataTable = new DataTable();
-        objDA.Fill(excelDataTable);
+        WorkSheet workSheet;
 
-        return excelDataTable;
+        if (tableName == DefaultTable)
+            workSheet = workBook.DefaultWorkSheet;
+        else
+            workSheet = workBook.GetWorkSheet(tableName);
+
+        if (workSheet is null)
+            return new DataTable();
+
+        await Task.CompletedTask;
+        return workSheet.ToDataTable(true);
     }
 
-    public static async Task<DataTable> ConvertExcelToDataTable(string path, string fileName, string tableName = DefaultTable)
-        => await ConvertExcelToDataTable(SplitedPathToFull(path, fileName), tableName);
+    public static async Task<DataTable> ConvertExcelToDataTableAsync(string path, string fileName, string tableName = DefaultTable)
+        => await ConvertExcelToDataTableAsync(SplitedPathToFull(path, fileName), tableName);
+    
+    public static IEnumerable<T> ConvertExcel<T>(string fullPath, string tableName = DefaultTable)
+        => ConvertExcelAsync<T>(fullPath, tableName).GetAwaiter().GetResult();
+
+    public static IEnumerable<T> ConvertExcel<T>(string path, string filename, string tableName = DefaultTable)
+        => ConvertExcelAsync<T>(path, filename, tableName).GetAwaiter().GetResult();
+
+
+    public static DataTable ConvertExcelToDataTable(string fullPath, string tableName = DefaultTable)
+        => ConvertExcelToDataTableAsync(fullPath, tableName).GetAwaiter().GetResult();
+
+    public static DataTable ConvertExcelToDataTable(string path, string fileName, string tableName = DefaultTable)
+        => ConvertExcelToDataTableAsync(path, fileName, tableName).GetAwaiter().GetResult();
 
     private static string SplitedPathToFull(string path, string fileName)
     {
@@ -47,14 +68,15 @@ public class ExcelRead
             
         string fullpath = string.Empty;
 
-        if (path.EndsWith('/'))
-            fullpath = $"{path}/{fileName}";
+        if (path.EndsWith('\\'))
+            fullpath = $"{path}\\{fileName}";
         else
             fullpath = $"{path}{fileName}";
 
         return fullpath;
     }
 
+    [Obsolete("OleDb is not used.")]
     private static string ConvertToConnectionStringOleDb(string fullPath)
     {
         if (string.IsNullOrWhiteSpace(fullPath))
@@ -73,5 +95,20 @@ public class ExcelRead
             throw new FormatException("File must be ends with '.xls' or '.xlsx'.");
 
         return connectionStringOleDb;
+    }
+
+    public static string SerializeDataTable(DataTable dataTable)
+    {
+        if (dataTable is null ||
+            !dataTable.AsEnumerable().Any())
+        {
+            return string.Empty;
+        }
+
+        var data = dataTable.Rows.OfType<DataRow>()
+                    .Select(row => dataTable.Columns.OfType<DataColumn>()
+                        .ToDictionary(col => col.ColumnName, c => row[c]));
+
+        return System.Text.Json.JsonSerializer.Serialize(data);
     }
 }
